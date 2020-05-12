f2f46d7ee683dc8cbf1f83861d506e978df1c6ed
Eduardo Domingos <epcdomingos@gmail.com>
2020-05-12 19:05:51 +0100

    WIP

--- js/script.js
+++ js/script.js
@@ -3,17 +3,46 @@
         var dom = {
             $window: $(window),
             $body: $('body'),
+            $siteContainer: $('.site-container'),
+            $themeToggle: $('.theme-toggle__button'),
+            $offcanvasToggle: $('.offcanvas-toggle'),
         };
 
         (function init() {
+            // Collapse Header
+            headerCollapser();
             dom.$window.scroll(function() {
-                if (getBodyScrollTop() > 200) {
-                    dom.$body.addClass('header-is-collapsed');
-                } else {
-                    dom.$body.removeClass('header-is-collapsed');
+                headerCollapser();
+            });
+            
+            // Toggle theme
+            dom.$themeToggle.click(function() {
+                dom.$body.toggleClass('theme--light theme--dark');
+                if(dom.$body.hasClass('theme--light')) {
+                    localStorage.setItem('theme', JSON.stringify({'mode':'theme--light'}));
+                }
+                if(dom.$body.hasClass('theme--dark')) {
+                    localStorage.setItem('theme', JSON.stringify({'mode':'theme--dark'}));
                 }
             });
+
+            // Toggle Offcanvas
+            dom.$offcanvasToggle.click(function() {
+                $(this).toggleClass('is-active');
+                dom.$body.toggleClass('no-scroll');
+                dom.$siteContainer.toggleClass('offcanvas-is-open');
+            });
+
+
         })();
+
+        function headerCollapser() {
+            if (getBodyScrollTop() > 200) {
+                dom.$siteContainer.addClass('header-is-collapsed');
+            } else {
+                dom.$siteContainer.removeClass('header-is-collapsed');
+            }
+        }
 
         function getBodyScrollTop () { 
             var el = document.scrollingElement || document.documentElement;
